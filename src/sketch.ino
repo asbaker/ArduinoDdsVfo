085329a219d6972c4373ded3e67eb65d5d42c4a5
Anthony Baker <anthony.s.baker@gmail.com>
2015-01-10 16:32:47 -0500

    Sweep functionality

--- src/sketch.ino
+++ src/sketch.ino
@@ -8,8 +8,15 @@
 long currentFrequency = DEFAULT_HZ;
 long newFrequency = 0;
 
+long startFrequency = 0;
+long stopFrequency = 0;
+long time = 0;
+long lastMillis = 0;
 
-AD9850 vfo; 
+
+AD9850 vfo;
+serLCD lcd(13);
+
 #define W_CLK 2
 #define FQ_UD 3
 #define DATA  4
@@ -17,7 +24,16 @@ AD9850 vfo;
 
 State notOscillating(notOscillatingEnter, NULL);
 State oscillating(oscillatingEnter, NULL);
-State frequencyInput(frequencyInputEnter, respondToFrequencyKeys);;
+State frequencyInput(frequencyInputEnter, respondToFrequencyKeys);
+
+// states for auto sweep
+// start freq -> end freq -> time -> auto sweep -> no oscillating
+State startFrequencyInput(startFrequencyEnter, respondToStartKeys);
+State endFrequencyInput(stopFrequencyEnter, respondToStopKeys);
+State timeInput(timeEnter, respondToTimeKeys);
+State autoSweep(autoSweepEnter, NULL);
+
+
 StateMachine stateMachine = StateMachine();
 
 
@@ -32,24 +48,23 @@ char keys[4][3] = {
 };
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, 4, 3);
 
-serLCD lcd(13);
-
 
 Button oscillateButton = Button(A0, BUTTON_PULLUP_INTERNAL);
 Button frequencyButton = Button(A1, BUTTON_PULLUP_INTERNAL);
+Button autoSweepButton = Button(A2, BUTTON_PULLUP_INTERNAL);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("starting setup");
-  vfo.setup(W_CLK, FQ_UD, DATA, RESET); 
+  vfo.setup(W_CLK, FQ_UD, DATA, RESET);
 
 
   keypad.addEventListener(keypadEvent);
+
   oscillateButton.clickHandler(oscillatePressed);
   frequencyButton.clickHandler(frequencyPressed);
+  autoSweepButton.clickHandler(autoSweepPressed);
 
   stateMachine.changeState(notOscillating);
-  Serial.println("done with setup");
 }
 
 void loop() {
@@ -57,7 +72,34 @@ void loop() {
 
   oscillateButton.isPressed();
   frequencyButton.isPressed();
+  autoSweepButton.isPressed();
+
+
+  if(stateMachine.is(autoSweep)) {
+    updateAutoSweep();
+  }
 }
+
+void updateAutoSweep() {
+  int hzPerSecond = (stopFrequency-startFrequency)/time;
+  int millisPassed = millis() - lastMillis; // thousands of a second
+  lastMillis = millis();
+
+  int addToFrequency = (hzPerSecond/1000.*millisPassed);
+  currentFrequency = currentFrequency + addToFrequency;
+
+  if(currentFrequency>stopFrequency) {
+    stateMachine.changeState(notOscillating);
+  }
+  else {
+    vfo.oscillate(currentFrequency);
+    updateLcd(currentFrequency);
+    delay(10);
+  }
+}
+
+
+/* ####### BUTTON INTERACTIONS ####### */
 
 void oscillatePressed(Button& button) {
   if(stateMachine.is(notOscillating)) {
@@ -72,22 +114,23 @@ void frequencyPressed(Button& button) {
   stateMachine.changeState(frequencyInput);
 }
 
+void autoSweepPressed(Button& button) {
+  stateMachine.changeState(startFrequencyInput);
+}
+
+
 /* ########## KEYPAD INTERACTIONS ######### */
 
 void keypadEvent(KeypadEvent key){
-  Serial.println("keypad event");
   if(keypad.getState() == PRESSED) {
-    Serial.print("key pressed: ");
-    Serial.println(key);
-
     stateMachine.sendKey(key);
   }
 }
 
-void respondToFrequencyKeys(char key) {
-  Serial.print("respondToFrequencyKeys: ");
-  Serial.println(key);
+// not sure how to best refactor these ....
+// curryable functions would be nice about right now
 
+void respondToFrequencyKeys(char key) {
   switch (key) {
     case '0':
     case '1':
@@ -99,7 +142,7 @@ void respondToFrequencyKeys(char key) {
     case '7':
     case '8':
     case '9':
-      newFrequency = newFrequency*10 + (int)key - (int)'0';
+      newFrequency = newFrequency*10 + getKeyInt(key);
       updateLcd(newFrequency);
       break;
     case '#':
@@ -109,28 +152,113 @@ void respondToFrequencyKeys(char key) {
   }
 }
 
+void respondToStartKeys(char key) {
+  switch (key) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      startFrequency = startFrequency*10 + getKeyInt(key);
+      updateStartLcd();
+      break;
+    case '#':
+      stateMachine.changeState(endFrequencyInput);
+      break;
+  }
+}
+
+void respondToStopKeys(char key) {
+  switch (key) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      stopFrequency = stopFrequency*10 + getKeyInt(key);
+      updateStopLcd();
+      break;
+    case '#':
+      stateMachine.changeState(timeInput);
+      break;
+  }
+}
+
+void respondToTimeKeys(char key) {
+  switch (key) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      time = time*10 + getKeyInt(key);
+      updateTimeLcd();
+      break;
+    case '#':
+      stateMachine.changeState(autoSweep);
+      break;
+  }
+}
+
+int getKeyInt(char key) {
+  return (int)key - (int)'0';
+}
+
 /* ########################################## */
 
 
 /* ######## STATE MANAGEMENT #########*/
 
 void notOscillatingEnter() {
-  Serial.println("entering oscillate off");
   vfo.stop(); 
   updateLcd(currentFrequency);
 }
 
 void oscillatingEnter() {
-  Serial.println("entering oscillate on");
   vfo.oscillate(currentFrequency);
   updateLcd(currentFrequency);
 }
 
 void frequencyInputEnter() {
-  Serial.println("frequency input enter");
   newFrequency = 0;
   updateLcd(newFrequency);
 }
+
+void startFrequencyEnter() {
+  startFrequency = 0;
+  updateStartLcd();
+}
+
+void stopFrequencyEnter() {
+  stopFrequency = 0;
+  updateStopLcd();
+}
+
+void timeEnter() {
+  time = 0;
+  updateTimeLcd();
+}
+
+void autoSweepEnter() {
+  currentFrequency=startFrequency;
+  lastMillis = millis();
+}
+
 
 /* ################################## */
 
@@ -150,14 +278,38 @@ void updateLcd(long freq) {
   if(stateMachine.is(frequencyInput)) {
     lcd.print("#-save");
   }
+  else if(stateMachine.is(autoSweep)) {
+    lcd.print("Auto Sweep...");
+  }
   else {
-    lcd.print("osc hz");
+    lcd.print("osc hz sweep");
   }
 }
 
 void printFrequency(long freq) {
   lcd.print(freq);
   lcd.selectLine(2);
+}
+
+void updateStartLcd() {
+  lcd.clear();
+  printFrequency(startFrequency);
+  lcd.print("Start Hz  #-save");
+}
+
+void updateStopLcd() {
+  lcd.clear();
+  printFrequency(stopFrequency);
+  lcd.print("End Hz    #-save");
+}
+
+void updateTimeLcd() {
+  lcd.clear();
+
+  lcd.print(time);
+  lcd.selectLine(2);
+
+  lcd.print("Seconds   #-save");
 }
 
 /* #################*/
